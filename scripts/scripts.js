40890ec887b5f13cb704d936fe019425f701b815
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-28 09:57:53 -0700

    Added vehicle arrays

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,44 @@
-console.log("hello");
+var vehicle1 = {
+    manufacturer: "Nissan",
+    model: "Altima",
+    year: 2010,
+    miles: 40000,
+    renter: "xXbossmanXx",
+    value: 35000
+  };
+
+var vehicle2 = {
+    manufacturer: "Toyota",
+    model: "Supra",
+    year: 1998,
+    miles: 14229,
+    renter: "lovely_duck",
+    value: 120000
+  };
+
+var vehicle3 = {
+    manufacturer: "Lamborghini",
+    model: "Aventador",
+    year: 2017,
+    miles: 45000,
+    renter: "wuTang",
+    value: 506300
+  };
+
+var vehicle4 = {
+    manufacturer: "Datsun",
+    model: "250z",
+    year: 1972,
+    miles: 50000,
+    renter: "oldman_jenkins420",
+    value: 42522
+  };
+
+var vehicle5 = {
+    manufacturer: "Audi",
+    model: "S3",
+    year: 2018,
+    miles: 13000,
+    renter: "biscuits'n'butters",
+    value: 43650
+  };
